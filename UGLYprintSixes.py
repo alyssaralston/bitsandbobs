d7c093a310d6b38b480b7781133f2b7a6482b4e0
alyssaralston <alyssaralston@gmail.com>
2013-05-05 20:17:08 -0700

    First real crack at Hackbright application printSixes task

---
+++ UGLYprintSixes.py
@@ -0,0 +1,43 @@
+def printSixes(numbers):
+   sixes = i + 6
+	 
+i = 6
+
+print "Here are multiples of six:"
+print i
+
+i = i + 6
+
+print "%d" % i
+
+i = i + 6
+
+print "%d" % i
+i = i + 6
+
+print "%d" % i
+i = i + 6
+
+print "%d" % i
+i = i + 6
+
+print "%d" % i
+i = i + 6
+
+print "%d" % i
+i = i + 6
+
+print "%d" % i
+i = i + 6
+
+print "%d" % i
+i = i + 6
+
+print "%d" % i
+i = i + 6
+
+print "%d" % i
+i = i + 6
+
+print "%d" % i
+i = i + 6
